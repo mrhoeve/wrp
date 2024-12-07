d77aff9e5edf5f7853da5f3713553c93e7893d68
Marc Hoeve <mrhoeve@gmail.com>
2024-12-07 08:53:56 +0100

    Release 1.5

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t mrhoeve/wrp:latest -t mrhoeve/wrp:1.5 .
-#docker push mrhoeve/wrp:1.4
-#docker push mrhoeve/wrp:latest
+docker push mrhoeve/wrp:1.5
+docker push mrhoeve/wrp:latest
